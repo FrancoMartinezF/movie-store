2ac720a9be22a456b2cd9b5d1fad3ee315b5a0d3
FrancoMartinezF <francomartinez2307@gmail.com>
2022-08-24 16:21:50 -0300

    frontend 2

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,30 +2,33 @@ import React from 'react';
 
 
 function Formulario() {
-    return ( 
-        <form>
-            <div className="mb-3">
-                <label for="titulo" className="form-label">Titulo</label>
-                <input type="text" className="form-control"/>
-            </div>
+    return (
+        <>
+            <h1>Agregar Pelicula</h1>
+            <form>
+                <div className="mb-3">
+                    <label for="titulo" className="form-label">Titulo</label>
+                    <input type="text" className="form-control"/>
+                </div>
 
-            <div class="mb-3">
-                <label for="sinopsis" class="form-label">Breve Sinopsis</label>
-                <textarea class="form-control" rows="3"></textarea>
-            </div>
+                <div class="mb-3">
+                    <label for="sinopsis" class="form-label">Breve Sinopsis</label>
+                    <textarea class="form-control" rows="3"></textarea>
+                </div>
 
-            <div className="mb-3">
-                <label for="año" className="form-label">Año</label>
-                <input type="number" className="form-control"/>
-            </div>
+                <div className="mb-3">
+                    <label for="año" className="form-label">Año</label>
+                    <input type="number" className="form-control"/>
+                </div>
 
-            <div class="input-group mb-3 mt-6">
-                <label className="input-group-text" for="imagen">Imagen</label>
-                <input type="file" class="form-control" />
-            </div>
+                <div class="input-group mb-3 mt-6">
+                    <label className="input-group-text" for="imagen">Imagen</label>
+                    <input type="file" class="form-control" />
+                </div>
 
-            <button type="submit" className="btn btn-primary">Agregar</button>
-        </form>
+                <button type="submit" className="btn btn-primary">Agregar</button>
+            </form>
+        </>
      );
 }
 
